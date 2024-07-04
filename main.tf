23112b0a2c49a31c839030b5bf21cab538eca001
Aditya-Deeparmor <142567644+Aditya-Deeparmor@users.noreply.github.com>
2024-07-04 16:59:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_secretsmanager_secret" "example" {
+  name        = "example-password"
+  description = "This is a sample password stored in AWS Secrets Manager"
+}
+
+resource "aws_secretsmanager_secret_version" "example" {
+  secret_id     = aws_secretsmanager_secret.example.id
+  secret_string = jsonencode({
+    password = var.secret_password
+  })
+}
+
+output "secret_arn" {
+  value = aws_secretsmanager_secret.example.arn
+}
+
+output "secret_id" {
+  value = aws_secretsmanager_secret.example.id
+}
